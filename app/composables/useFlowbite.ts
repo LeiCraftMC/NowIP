b30dbc8771cbef2551470aeaa2e8dd2921396efb
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-08-17 02:33:36 +0200

    update

---
+++ app/composables/useFlowbite.ts
@@ -0,0 +1,8 @@
+
+export function useFlowbite(callback: any) {
+    if (process.client) {
+        import('flowbite').then((flowbite) => {
+            callback(flowbite);
+        });
+    }
+}
---
+++ app/layouts/dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="app">
+        <Sidebar class="sidebar">
+            <main class="content">
+                <div class="flex-grow-1">
+                    <slot />
+                </div>
+                <Footer :withMargin="true" />
+            </main>
+        </Sidebar>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+    import Sidebar from '@/components/Sidebar.vue';
+</script>
+
+<style scoped>
+
+.app {
+    font-family: "Rubik", sans-serif;
+    background-color: #0b0c1b;
+    color: white;
+    font-size: 16px;
+    margin: 0;
+    padding: 0; 
+    min-height: 100vh;
+    word-wrap: break-word;
+    display: flex;
+    flex-direction: column;
+}
+
+.sidebar {
+    /* margin-top: 6rem; */
+    flex-grow: 1;
+
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+}
+
+</style>
