9a81bdded98428d09029bf1ba880db37012332ea
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-08-22 15:58:55 +0200

    update

---
+++ app/layouts/dashboard.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+export const description = "An inset sidebar with secondary navigation."
+export const iframeHeight = "800px"
+</script>
+
+<script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { Separator } from '@/components/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <slot />
+    </SidebarInset>
+  </SidebarProvider>
+</template>
